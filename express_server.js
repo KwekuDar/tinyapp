c9699201c099519abb2a418b534fd7149a95e2d2
KwekuDar <kwekudar@outlook.com>
2021-12-08 16:34:15 -0500

    added redirection from short url to long url

--- express_server.js
+++ express_server.js
@@ -51,4 +51,9 @@ app.post("/urls", (req, res) => {
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 
