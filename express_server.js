e79c0a08c44f599aa125c6558d3d98d63783f994
KwekuDar <kwekudar@outlook.com>
2021-12-12 15:09:17 -0500

    all functionality finished

--- express_server.js
+++ express_server.js
@@ -8,19 +8,25 @@ const PORT = 8080;
 app.set("view engine", "ejs");
 
 //Password Hash
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 //Body Parser
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 //Cookies
-const cookieParser = require("cookie-parser");
-app.use(cookieParser());
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2'],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": { longURL:"http://www.lighthouselabs.ca", userID:"aJ48lW"},
+  "9sm5xK": { longURL:"http://www.google.com", userID: "aJ48lW"}
 };
 
 const users = { 
@@ -70,43 +76,50 @@ app.get("/login", (req, res) => {
 
 app.post("/register", (req, res) => {
   const userId = generateRandomString();
-  let email = req.body.email;
-  let password = req.body.password;
-  console.log(emailExists(email));
+  const email = req.body.email;
+  const password = req.body.password;
+  const hashedPassword = bcrypt.hashSync(password, 10);
   if (email === "" || password === "") {
     res.status(400).send('<h1>Error!</h1> <p>You need to enter values for email and password.</p>');
   } if (emailExists(email) === true) {
     res.status(400).send('<h1>Error!</h1> <p>This email is already associated with an account. Try another one.</p>');
   } if ((emailExists(email)) === false) {
-    users[userId] = {"id": userId , "email": req.body.email, "password": req.body.password,};
-    res.cookie('user_id', userId);
+    users[userId] = {"id": userId , "email": req.body.email, "password": hashedPassword,};
+    req.session.user_id = userId;
     console.log(users);
     res.redirect("/urls");
   }
 });
 
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase, user: users[req.cookies["user_id"]]};
+    const templateVars = { urls: urlDatabase, user: users[req.session.user_id]};
     res.render("urls_index", templateVars);
   });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies["user_id"]] };
-  res.render("urls_new", templateVars);
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.session.user_id] };
+  if (users[req.session.user_id]) {
+    res.render("urls_new", templateVars);
+  } else {
+    res.redirect('/login');
+  }
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies["user_id"]]};
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL, user: users[req.session.user_id], urls: urlDatabase };
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const longURL = req.body.longURL;
+  const userID = req.session.user_id;
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = { 'longURL': longURL, userID: userID };
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });
 
@@ -121,20 +134,19 @@ app.post("/urls/:id", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  let email = req.body.email;
-  let password = req.body.password;
+  const email = req.body.email;
+  const password = req.body.password;
   let userKeys = Object.keys(users);
-  console.log(emailExists(email));
   if (email === "" || password === "") {
     res.status(403).send('<h1>Error!</h1> <p>You need to enter values for email and password.</p>');
   } if (emailExists(email) === false) {
     res.status(403).send('<h1>Error!</h1> <p>This email is not associated with an account. Register for an account if you do not already have one.</p>');
   } if ((emailExists(email)) === true) {
       for (key of userKeys) {
-        if (users[key]["password"] === password) {
-          res.cookie('user_id', users[key]["id"]);
+        if (bcrypt.compareSync(password, users[key]["password"])) {
+          req.session.user_id = users[key]["id"];
           res.redirect("/urls");
-        } if (users[key]["password"] !== password) {
+        } else {
           res.status(403).send('<h1>Error!</h1> <p>Password is incorrect.</p>');
         }
       }
@@ -142,7 +154,7 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('user_id');
+  req.session = null;
   res.redirect("/urls");  
 });
 
